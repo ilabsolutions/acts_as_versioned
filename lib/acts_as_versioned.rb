8e205cbbad004dcdea54dd80466f6e4250d16c4b
Rishab Govind <rishab.govind@ilabsolutions.com>
2013-12-06 13:45:56 -0500

    Index name should not be longer than 64 chars.

--- lib/acts_as_versioned.rb
+++ lib/acts_as_versioned.rb
@@ -455,7 +455,9 @@ module ActiveRecord #:nodoc:
                                          :precision => type_col.precision
             end
 
-            self.connection.add_index versioned_table_name, versioned_foreign_key
+            name = 'index_' + versioned_table_name + '_on_' + versioned_foreign_key
+            self.connection.add_index versioned_table_name, versioned_foreign_key, :name => name[0,63]
+
           end
 
           # Rake migration task to drop the versioned table
